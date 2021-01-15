5e4dc9a48517048dcd13474bc9eb7570efb44276
Musab Hamid <47342534+musab118@users.noreply.github.com>
2021-01-14 18:58:36 -0600

    Initial Commit

---
+++ src/app/components/todos/todos.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { fromEventPattern } from 'rxjs';
+import {Todo } from '../../models/Todo'
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit {
+  todos:Todo[];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.todos = [
+      {
+        id: 1,
+        title: 'First Task',
+        completed:false
+
+      },
+      {
+        id: 2,
+        title: 'Second Task',
+        completed:false
+
+      },
+      {
+        id: 3,
+        title: 'Third Task',
+        completed:false
+
+      },
+      
+    ]
+  }
+
+}
